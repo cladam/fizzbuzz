5aaf721384c334d9ae3a1ddb9cf7f6912b4a8260
Claes Adamsson <claes.adamsson@ikea.com>
2013-12-19 22:56:18 +0100

    Added ruby version

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+(1..100).each do |n|
+  if n % 3 == 0
+    puts "Fizz"
+  elsif n % 5 == 0
+    puts "Buzz"
+  elsif n % 15 == 0
+    puts "FizzBuzz"
+  else
+    puts n
+  end
+end
